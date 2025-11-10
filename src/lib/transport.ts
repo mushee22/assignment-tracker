3bad5e195a1a9d21e3b1e5722180de437f98674d
mushee22 <mmtmusheer@gmail.com>
2025-11-10 16:24:00 +0530

    fix(transport): hardcode secure flag to true for mailer

--- src/lib/transport.ts
+++ src/lib/transport.ts
@@ -1,7 +1,7 @@
 export const transport = {
   host: process.env.MAILER_HOST,
   port: Number(process.env.MAILER_PORT),
-  secure: process.env.MAILER_SECURE === 'true',
+  secure: true,
   auth: {
     user: process.env.MAILER_USER,
     pass: process.env.MAILER_PASS,
